13636d9f92ab3c27193f472f0850ce01234c1245
@lucasgvdii <72617878+Lucasgvdii@users.noreply.github.com>
2020-10-13 11:46:05 +0200

    Update daily_sales_statistics.py

--- daily_sales_statistics.py
+++ daily_sales_statistics.py
@@ -29,22 +29,14 @@ def daily_sales_statistics(daily_sales_list):
         if i[0]=="A":
             lunch_sale=i[1]
 
-        total_v_sales=total_v_sales+i[1]
+    total_v_sales=total_v_sales+i[1]
 
-        if i[0]=="N":
-            avg_sales=total_v_sales/5
-            statistics=statistics+get_day(max_meal)+get_day(min_meal)
-            if avg_sales<lunch_sale:
-                statistics=statistics+"Yes\n"
-            else:
-                statistics=statistics+"No\n"
-                
-            lunch_sale=0
-            total_v_sales=0
-            min_meal=""
-            min_meal_sale=0
-            max_meal=""
-            max_meal_sale=0
+    avg_sales=total_v_sales/5
+    statistics=statistics+get_day(max_meal)+get_day(min_meal)
+    if avg_sales<lunch_sale:
+        statistics=statistics+"Yes"
+    else:
+        statistics=statistics+"No"
             
     return statistics
                 
@@ -65,5 +57,5 @@ def get_day(day):
                 
         
 #You can obviously print these inside the function, but i personally like to keep the code "print-free"
-print(daily_sales_statistics([["D",2.8],["C",48.0],["A",8.0],["N",0]]))                                                         # --> cocktails#tie#No
-print(daily_sales_statistics([["D",2.8],["C",48.0],["A",8.0],["N",0],["D",15.33],["A",60.0],["M",12.0],["I",25.0],["N",0]]))    # --> cocktails#tie#No lunch#tie#Yes
+print(daily_sales_statistics([["D",2.8],["C",48.0],["A",8.0],["N",0]]))                  # --> cocktails#tie#No
+print(daily_sales_statistics([["D",15.33],["A",60.0],["M",12.0],["I",25.0],["N",0]]))    # --> lunch#tie#Yes
