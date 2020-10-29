b1a7bcbab8208aa37fd7a35c3da91f5ef7305895
@lucasgvdii <72617878+Lucasgvdii@users.noreply.github.com>
2020-10-29 13:02:57 +0100

    Update vampire_numbers.py

--- vampire_numbers.py
+++ vampire_numbers.py
@@ -8,6 +8,14 @@ def is_vampire(num):
     return get_num(num,num_len/2,"","",num2list(num))
 
 #Main auxiliar function
+"""I've opted for a recursive function that takes digits from the original number to form all possible combinations of
+   numbers of length L="original_number_length/2" with them. As it get each of these combinations of length L, the 
+   function will multiply them by the possible combinations of length L that can be created with the remaining digits
+   of the original number (as we had only used "original_number_length/2" digits to make the first one). If the result
+   is   equal to the original number it will return True (as it would be a vampire number). If it isn't, it will continue 
+   searching and checking the remaining combinations until he finds one that fits or until it validates every posible
+   combination without succes (which would mean it's not a vampire number). To avoid repeating multiplications that have
+   already been done, it will only multiply those in which the first combination is greater than the second one."""
 def get_num(original_num,length,num1,num2,nums_left):
     total=[]
     if len(num1)==length:
