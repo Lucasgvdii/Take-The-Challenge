0e257b5e77a6557ef6ff5398b71cc275d16951dc
Lucas García de Viedma Pérez <72617878+Lucasgvdii@users.noreply.github.com>
2020-11-10 08:50:56 +0100

    Update zeros_in_factorial.py

---
+++ solutions_code/zeros_in_factorial.py
@@ -0,0 +1,12 @@
+#The factorial's zeros problem. Link to the explanation and heading of the problem --> https://www.aceptaelreto.com/problem/statement.php?id=138
+
+#Main function
+def get_zeros_in_factorial(num):
+
+    return num//5
+
+#You can obviously print these inside the function, but i personally to keep the code "print-free"
+print(get_zeros_in_factorial(3))
+print(get_zeros_in_factorial(5))
+print(get_zeros_in_factorial(10))
+print(get_zeros_in_factorial(24))
