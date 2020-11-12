1268ebcdbcae9242cab0b039cac9e638fe5ac2b7
Lucas García de Viedma Pérez <72617878+Lucasgvdii@users.noreply.github.com>
2020-11-12 09:28:38 +0100

    Update zeros_in_factorial.py

--- solutions_code/zeros_in_factorial.py
+++ solutions_code/zeros_in_factorial.py
@@ -2,11 +2,10 @@
 
 #Main function
 def get_zeros_in_factorial(num):
-
     return num//5
 
 #You can obviously print these inside the function, but i personally to keep the code "print-free"
-print(get_zeros_in_factorial(3))
-print(get_zeros_in_factorial(5))
-print(get_zeros_in_factorial(10))
-print(get_zeros_in_factorial(24))
+print(get_zeros_in_factorial(3))  # --> 0
+print(get_zeros_in_factorial(5))  # --> 1
+print(get_zeros_in_factorial(10)) # --> 2
+print(get_zeros_in_factorial(24)) # --> 4
