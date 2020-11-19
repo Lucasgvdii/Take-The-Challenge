c8588c9a5dea05f14a0e3347542c0cb4f9db14f3
Lucas García de Viedma Pérez <72617878+Lucasgvdii@users.noreply.github.com>
2020-11-19 14:03:32 +0100

    Update san_fermines_bulls.py

---
+++ solutions_code/san_fermines_bulls.py
@@ -0,0 +1,9 @@
+#The san fermines bulls problem. Link to the explanation and heading of the problem --> https://www.aceptaelreto.com/problem/statement.php?id=149
+
+#Main function
+def get_necessary_speed(bulls_speed):
+    return max(bulls)
+                   
+#You can obviously print these inside the function, but i personally to keep the code "print-free"
+print(get_necessary_speed([7,1,9,8,7,10,3,12])) # --> 12
+print(get_necessary_speed([1,10]))              # --> 10
